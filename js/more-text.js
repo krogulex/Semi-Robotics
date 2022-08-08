677e84536ffa16cc5103c014294fffdc1c0b0a4a
jurasw <57913219+jurasw@users.noreply.github.com>
2022-08-08 12:24:01 +0200

    text

--- js/more-text.js
+++ js/more-text.js
@@ -2,10 +2,8 @@ var status = "less";
 
 function toggleText()
 {
-    var text="Urządzenie dostosowuje się do osoby karmionej, jej postawy oraz tempa spożywania posiłku. Robot Karmiący to połączenie najnowszej technologii, sztucznej inteligencji oraz zaawansowanego oprogramowania.";
-
     if (status == "less") {
-        document.getElementById("about__text_extended").innerHTML=text;
+        document.getElementById("about__text_extended").innerHTML = "Urządzenie dostosowuje się do osoby karmionej, jej postawy oraz tempa spożywania posiłku. Robot Karmiący to połączenie najnowszej technologii, sztucznej inteligencji oraz zaawansowanego oprogramowania.";
         document.getElementById("about__button").innerText = "Mniej";
         status = "more";
     } else if (status == "more") {
